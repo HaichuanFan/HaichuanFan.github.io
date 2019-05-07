42d00ca34b232f0aa6ecb2e34d26236ba1e0e383
HaichuanFan <fanhaichuan1020@sina.com>
2019-05-07 16:35:47 +0800

    Site updated: 2019-05-07 16:35:47

--- js/time.js
+++
@@ -1,4 +0,0 @@
-<!--人体时钟背景透明-->
-<script charset="Shift_JIS" src="http://chabudai.sakura.ne.jp/blogparts/honehoneclock/honehone_clock_tr.js"></script>
-<!--人体时钟背景白-->
-<script charset="Shift_JIS" src="http://chabudai.sakura.ne.jp/blogparts/honehoneclock/honehone_clock_wh.js"></script>
